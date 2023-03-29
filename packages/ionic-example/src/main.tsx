322dc9beef336df68996ec5e4109059324fbe642
Stafford Williams <stafford.williams@gmail.com>
2023-03-29 17:05:26 +1100

    remove router

--- packages/ionic-example/src/main.tsx
+++ packages/ionic-example/src/main.tsx
@@ -1,5 +1,4 @@
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonApp, setupIonicReact } from '@ionic/react';
 import '@ionic/react/css/core.css';
 import '@ionic/react/css/display.css';
 import '@ionic/react/css/flex-utils.css';
@@ -12,7 +11,6 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/typography.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Redirect, Route } from 'react-router';
 import App from './App';
 import './index.css';
 import './variables.css';
@@ -22,12 +20,7 @@ setupIonicReact();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <IonApp>
-      <IonReactRouter>
-        <IonRouterOutlet>
-          <Route path="/home" component={App} />
-          <Redirect exact from="/" to="/home" />
-        </IonRouterOutlet>
-      </IonReactRouter>
+      <App />
     </IonApp>
   </React.StrictMode>
 );
