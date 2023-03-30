9b4cdcb5020b2ba40c790ed0bc65f8c48a496b29
Stafford Williams <stafford.williams@gmail.com>
2023-03-30 12:45:54 +1100

    don't re-export react-hook-form

---
+++ .changeset/mean-brooms-talk.md
@@ -0,0 +1,7 @@
+---
+'@makerx/forms-ionic': major
+---
+
+No longer re-exports react-hook-form.
+
+Consumers should now import react-hook-form directly.
--- packages/ionic/src/index.ts
+++ packages/ionic/src/index.ts
@@ -1,5 +1,4 @@
 import { useFieldMetaData, ValidatedFormFactory, z, zfd } from '@makerx/forms-core';
-import { Controller, useFormContext } from 'react-hook-form';
 import type { FormItemProps } from './components/form-item/FormItem';
 import { SubmitButton } from './components/submit-button/SubmitButton';
 import { ValidationErrorMessage } from './components/validation-error-message/ValidationErrorMessage';
@@ -7,6 +6,6 @@ import { FormFieldHelper as FormFieldHelperBase } from './util/FormFieldHelper';
 
 const ValidatedForm = ValidatedFormFactory(FormFieldHelperBase);
 
-export { ValidatedForm, FormFieldHelperBase, zfd, z, ValidatedFormFactory, useFieldMetaData, useFormContext, ValidationErrorMessage, SubmitButton, Controller };
+export { ValidatedForm, FormFieldHelperBase, zfd, z, ValidatedFormFactory, useFieldMetaData, ValidationErrorMessage, SubmitButton };
 export type { FormItemProps };
 
