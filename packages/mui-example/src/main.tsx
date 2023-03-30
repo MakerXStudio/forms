1d2a759525c4cc33a317b469c0c158253da32e90
Stafford Williams <stafford.williams@gmail.com>
2023-03-30 14:55:31 +1100

    mui-example: dark mode

--- packages/mui-example/src/main.tsx
+++ packages/mui-example/src/main.tsx
@@ -1,14 +1,43 @@
+import {
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  useMediaQuery,
+} from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { enAU } from 'date-fns/locale';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const Providers = ({ children }: PropsWithChildren) => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enAU}>
+        {children}
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enAU}>
+    <Providers>
       <App />
-    </LocalizationProvider>
+    </Providers>
   </React.StrictMode>
 );
